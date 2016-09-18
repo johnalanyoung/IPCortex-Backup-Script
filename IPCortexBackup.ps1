94fee55e814b861aa6dfda75feeef7720b21e736
johnalanyoung <johnalanyoung@hotmail.com>
2016-09-18 21:53:03 +0100

    Backup Directory Changes and Unique Names
    Better directory scheme for multiple systems.
    Unique identifier for each IPCortex.

--- IPCortexBackup.ps1
+++ IPCortexBackup.ps1
@@ -5,7 +5,8 @@
 
 Function Backup-Cortex{
     param  (
-            [string] $CortexAddress, 
+            [string] $Customer,
+            [string] $IPCortexAddress, 
             [string] $username,
             [string] $password
            )
@@ -13,11 +14,12 @@ Function Backup-Cortex{
             $date = Get-Date -Format "yyyy-MM-dd HHmmss" # Creates date and time which can be used for part of the filename
             $date2 = Get-Date -Format "yyyyMd" # Used for downloading voicemail greetings and system logs
             $BackupLocation = "X:\IPCortexBackups" # Root backup location
-            $BackupLocationSysConfig = "X:\IPCortexBackups\1 - System Config and Call Records" # System configuration and call records backup location
-            $BackupLocationIVR = "X:\IPCortexBackups\2 - IVR Sound Files" # IVR sound files backup location
-            $BackupLocationVMGreet = "X:\IPCortexBackups\3 - Voicemail Greetings" # Voicemail greetings backup location
-            $BackupLocationSysLogs = "X:\IPCortexBackups\4 - System Logs" # System logs backup location
-            $BackupLocationCallRec = "X:\IPCortexBackups\5 - Call Recordings" # Call recordings backup location
+            $BackupLocationCustomer = "$BackupLocation\$Customer"
+            $BackupLocationSysConfig = "$BackupLocationCustomer\1 - System Config and Call Records" # System configuration and call records backup location
+            $BackupLocationIVR = "$BackupLocationCustomer\2 - IVR Sound Files" # IVR sound files backup location
+            $BackupLocationVMGreet = "$BackupLocationCustomer\3 - Voicemail Greetings" # Voicemail greetings backup location
+            $BackupLocationSysLogs = "$BackupLocationCustomer\4 - System Logs" # System logs backup location
+            $BackupLocationCallRec = "$BackupLocationCustomer\5 - Call Recordings" # Call recordings backup location
             $tempfile = "$BackupLocation\tmp.txt" # Temporary file filename and location
             $cookie = "$BackupLocation/ipcortex_cookie.txt" # Cookie filename and file location
             $tempuser = [System.Net.WebUtility]::UrlEncode($username) # Encodes the username in case special characters are used to ensure complicated usernames work
@@ -28,24 +30,32 @@ Function Backup-Cortex{
 
             # Calls on the HTTP/S function check
             HTTPS-Check #Checks if the server is HTTP or HTTPS
-
+	
             # Sets the correct link for the download depending on the port that is used
             if ($Https = 1){
-            $CortexLoginURL = "https://$CortexAddress/login.whtm?sessionUser=$tempuser&sessionPass=$temppassword" # Creates login URL
-            $CortexDownloadURL = "https://$CortexAddress/admin/backup.whtm/update/backup.tar.gz" # Creates system configuration and call records download URL
-            $CortexIVRDownloadURL = "https://$CortexAddress/admin/backup.whtm/update/ivr.tar.gz" # Creates IVR sound files download URL
-            $CortexVMGreetDownloadURL = "https://$CortexAddress/admin/backup.whtm/vmgreet$date2.tar.gz" # Creates voicemail greetings download URL
-            $CortexLogDownloadURL = "https://$CortexAddress/admin/backup.whtm/logs$date2.tar.gz" # Creates system logs download URL
-            $CortexCallRecordingURL = "https://$CortexAddress/link/monitor.whtm" # Creates call recordings download URL
+            $CortexLoginURL = "https://$IPCortexAddress/login.whtm?sessionUser=$tempuser&sessionPass=$temppassword" # Creates login URL
+            $CortexDownloadURL = "https://$IPCortexAddress/admin/backup.whtm/update/backup.tar.gz" # Creates system configuration and call records download URL
+            $CortexIVRDownloadURL = "https://$IPCortexAddress/admin/backup.whtm/update/ivr.tar.gz" # Creates IVR sound files download URL
+            $CortexVMGreetDownloadURL = "https://$IPCortexAddress/admin/backup.whtm/vmgreet$date2.tar.gz" # Creates voicemail greetings download URL
+            $CortexLogDownloadURL = "https://$IPCortexAddress/admin/backup.whtm/logs$date2.tar.gz" # Creates system logs download URL
+            $CortexCallRecordingURL = "https://$IPCortexAddress/link/monitor.whtm" # Creates call recordings download URL
             }
             if ($Https = 0){
-            $CortexLoginURL = "http://$CortexAddress/login.whtm?sessionUser=$tempuser&sessionPass=$temppassword" # Creates login URL
-            $CortexDownloadURL = "http://$CortexAddress/admin/backup.whtm/update/backup.tar.gz" # Creates system configuration and call records download URL
-            $CortexIVRDownloadURL = "http://$CortexAddress/admin/backup.whtm/update/ivr.tar.gz" # Creates IVR sound files download URL
-            $CortexVMGreetDownloadURL = "http://$CortexAddress/admin/backup.whtm/vmgreet$date2.tar.gz" # Creates voicemail greetings download URL
-            $CortexLogDownloadURL = "http://$CortexAddress/admin/backup.whtm/logs$date2.tar.gz" # Creates system logs download URL
-            $CortexCallRecordingURL = "http://$CortexAddress/link/monitor.whtm" # Creates call recordings download URL
+            $CortexLoginURL = "http://$IPCortexAddress/login.whtm?sessionUser=$tempuser&sessionPass=$temppassword" # Creates login URL
+            $CortexDownloadURL = "http://$IPCortexAddress/admin/backup.whtm/update/backup.tar.gz" # Creates system configuration and call records download URL
+            $CortexIVRDownloadURL = "http://$IPCortexAddress/admin/backup.whtm/update/ivr.tar.gz" # Creates IVR sound files download URL
+            $CortexVMGreetDownloadURL = "http://$IPCortexAddress/admin/backup.whtm/vmgreet$date2.tar.gz" # Creates voicemail greetings download URL
+            $CortexLogDownloadURL = "http://$IPCortexAddress/admin/backup.whtm/logs$date2.tar.gz" # Creates system logs download URL
+            $CortexCallRecordingURL = "http://$IPCortexAddress/link/monitor.whtm" # Creates call recordings download URL
             }
+
+            # Create missing folders
+            md -Force "$BackuplocationCustomer"
+            md -Force "$BackupLocationSysConfig"
+            md -Force "$BackupLocationIVR"
+            md -Force "$BackupLocationVMGreet"
+            md -Force "$BackupLocationSysLogs"
+            md -Force "$BackupLocationCallRec"
 
             # The Downloads:
             # Starts first download and logs into IPCortex - Requires 2 max re-directs over http/s
@@ -61,17 +71,17 @@ Function Backup-Cortex{
             # Downloads call recordings - Requires 1 max re-directs over http/s
             .\wget.exe --no-check-certificate -O $tempfile --max-redirect=1 --load-cookies=$cookie --tries=1 $CortexCallRecordingURL
             $CortexCallRecordFile = (gc $tempfile | % { if($_ -match "_default.cgi/recorded.tar.gz") {$_.substring(13,$_.length-13-37)}})
-            $CortexCallRecordFileUrl = "http://$CortexAddress$CortexCallRecordFile"
+            $CortexCallRecordFileUrl = "http://$IPCortexAddress$CortexCallRecordFile"
             .\wget.exe -O $BackupLocationCallRec\"$date-recordings.tar.gz" --max-redirect=1 --load-cookies=$cookie --tries=1 $CortexCallRecordFileUrl
 
             # Deletes temporary file and cookie
             rm $tempfile
             rm $cookie
-            }
+		    }
 
 Function HTTPS-Check($Https){
             # Tries a download via HTTP access
-            $CortexLoginURL = "http://$CortexAddress/login.whtm?sessionUser=$tempuser&sessionPass=$temppassword" # Creates login URL
+            $CortexLoginURL = "http://$IPCortexAddress/login.whtm?sessionUser=$tempuser&sessionPass=$temppassword" # Creates login URL
             # Starts first download and logs into IPCortex - Requires 2 max re-directs over http/s
             .\wget.exe --no-check-certificate -O $tempfile --max-redirect=2 --save-cookies=$cookie --tries=1 $CortexLoginURL
 	
@@ -79,7 +89,7 @@ Function HTTPS-Check($Https){
             If ((Get-Content "$tempfile") -eq $Null){
 
             # Tries a download via HTTPS access
-            $CortexLoginURL = "https://$CortexAddress/login.whtm?sessionUser=$tempuser&sessionPass=$temppassword" # Creates login URL
+            $CortexLoginURL = "https://$IPCortexAddress/login.whtm?sessionUser=$tempuser&sessionPass=$temppassword" # Creates login URL
             # Starts first download and logs into IPCortex - Requires 2 max re-directs over http/s			
             .\wget.exe --no-check-certificate -O $tempfile --max-redirect=2 --save-cookies=$cookie --tries=1 $CortexLoginURL
 
@@ -97,6 +107,6 @@ Function HTTPS-Check($Https){
             }
 
             # Hostname and login credentials for IPCortex stored in seperate csv file. User will need relevant permissions needed to pull backups
-            Import-CSV "X:\IPCortexBackups\Project_Files\Main File\ipcortex_list.csv" -Header IP1,IP2,IP3 | Foreach-Object{
-                Backup-Cortex -CortexAddress $_.IP1 -username $_.IP2 -password $_.IP3
+            Import-CSV "X:\IPCortexBackups\Project_Files\Main File\ipcortex_list.csv" -Header Customer,Hostname,Username,Password | Foreach-Object{
+                Backup-Cortex -Customer $_.Customer -IPCortexAddress $_.Hostname -username $_.Username -password $_.Password
             }
